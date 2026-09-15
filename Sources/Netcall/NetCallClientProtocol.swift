833e803a1b9029ec2a47201b6a457bd0e2856922
Yann Bonafons <yann.bonafons@gmail.com>
2026-09-15 23:16:28 +0200

    split protocol and add protocol for image fetching

--- Sources/Netcall/NetCallClientProtocol.swift
+++ Sources/Netcall/NetCallClientProtocol.swift
@@ -5,9 +5,9 @@
 //  Created by Yann Bonafons on 05/04/2026.
 //
 
-import Foundation
+import UIKit
 
-public protocol NetCallClientProtocol: Sendable {
+public protocol NetCallClientProtocol {
     /// Fetch
     func fetchRemoteData<T: Codable & Sendable>(requestInfo: NetCallRequestInfo, decoder: JSONDecoder) async throws -> T
 
@@ -17,18 +17,8 @@ public protocol NetCallClientProtocol: Sendable {
     /// Fire-and-forget (204, etc.)
     func request(requestInfo: NetCallRequestInfo) async throws
 
-    /// Header
-    func updateSharedHeaders(_ headers: [String: String]) async
-    func setSharedHeader(name: String, value: String?) async
-
-    /// Base URL
-    func updateBaseURL(_ baseURL: String?) async
-
     /// Request lifecycle
     func cancelAll() async
-
-    /// Auth
-    func setUnauthorizedRefreshHook(_ hook: NetCallUnauthorizedRefreshHook?) async
 }
 
 extension NetCallClientProtocol {
@@ -36,4 +26,26 @@ extension NetCallClientProtocol {
     public func fetchRemoteData<T: Codable & Sendable>(requestInfo: NetCallRequestInfo) async throws -> T {
         try await fetchRemoteData(requestInfo: requestInfo, decoder: JSONDecoder())
     }
+}
+
+public protocol NetCallConfigurationProtocol {
+    /// Confugure  all headers
+    func updateSharedHeaders(_ headers: [String: String]) async
+    
+    /// Set a specific header
+    func setSharedHeader(name: String, value: String?) async
+    
+    /// Set a specific header for image fetching
+    func setImageSharedHeader(name: String, value: String?) async
+    
+    /// Set base URL (domain use on all NetCallClientProtocol requests)
+    func updateBaseURL(_ baseURL: String?) async
+    
+    /// Set a refresh hook that will be called on a 401 error. This is the opportunity to refresh an auth token
+    func setUnauthorizedRefreshHook(_ hook: NetCallUnauthorizedRefreshHook?) async
+}
+
+public protocol NetCallImageClientProtocol {
+    /// Fetch an image. The fetched image will be store in cache (and optionnaly on the disk)
+    func fetchImage(from urlString: String, useDisk: Bool) async -> UIImage?
 }
